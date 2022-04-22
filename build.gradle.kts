11983369828ba2161230c7d17aea3ba93d157fe0
dominik <20093701@mail.wit.ie>
2022-04-22 12:28:05 +0100

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+    application
+}
+
+group = "me.domin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    // dependencies for logging
+    implementation("io.github.microutils:kotlin-logging:2.1.15")
+    implementation("org.slf4j:slf4j-simple:1.7.32")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,60 @@
+import mu.KotlinLogging
+import utils.ScannerInput
+import java.lang.System.exit
+
+private val logger = KotlinLogging.logger {}
+
+fun main(args: Array<String>) {
+    runMenu()
+}
+
+fun mainMenu() : Int {
+    return ScannerInput.readNextInt(""" 
+         > ----------------------------------
+         > |        BOOK RATING APP         |
+         > ----------------------------------
+         > | BOOK RATING MENU               |
+         > |   1) Add a book                |
+         > |   2) List all books            |
+         > |   3) Update a book             |
+         > |   4) Delete a book             |
+         > ----------------------------------
+         > |   0) Exit                      |
+         > ----------------------------------
+         > ==>> """.trimMargin(">"))
+}
+
+fun runMenu() {
+    do {
+        val option = mainMenu()
+        when (option) {
+            1  -> addBook()
+            2  -> listBooks()
+            3  -> updateBook()
+            4  -> deleteBook()
+            0  -> exitApp()
+            else -> System.out.println("Invalid option entered: " + option)
+        }
+    } while (true)
+}
+
+fun addBook(){
+    logger.info { "addBook() function invoked" }
+}
+
+fun listBooks(){
+    logger.info { "listBooks() function invoked" }
+}
+
+fun updateBook(){
+    logger.info { "updateBook() function invoked" }
+}
+
+fun deleteBook(){
+    logger.info { "deleteBook() function invoked" }
+}
+
+fun exitApp(){
+    println("Exiting...bye")
+    exit(0)
+}
