36dc30e377b2d0de4339481887b2d472b53f0da3
dominik <20093701@mail.wit.ie>
2022-04-29 22:02:37 +0100

    JUnit Testing - AddBooks, UpdateBooks

--- src/test/kotlin/controllers/BookAPITest.kt
+++ src/test/kotlin/controllers/BookAPITest.kt
@@ -6,28 +6,32 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import persistence.JSONSerializer
+import persistence.XMLSerializer
 import java.io.File
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
+
 class BookAPITest {
+
     private var wonder: Book? = null
     private var noLongerHuman: Book? = null
     private var rollOfThunder: Book? = null
     private var wimpyKid1: Book? = null
     private var gatsby: Book? = null
-    private var populatedBooks: BookAPI? = BookAPI(JSONSerializer(File("books.json")))
-    private var emptyBooks: BookAPI? = BookAPI(JSONSerializer(File("books.json")))
+    private var populatedBooks: BookAPI? = BookAPI(XMLSerializer(File("books.xml")))
+    private var emptyBooks: BookAPI? = BookAPI(XMLSerializer(File("books.xml")))
 
     @BeforeEach
     fun setup() {
-        wonder = Book("Wonder", "R. J. Palacio", "Ficton", 2012, 315, 9.99, true)
-        noLongerHuman = Book("No Longer Human", "Osamu Dazai", "Fiction", 1948, 170, 7.99, true)
-        rollOfThunder = Book("Roll Of Thunder, Hear My Cry", "Mildred D. Taylor", "Fiction", 1976, 220, 8.99, true)
-        wimpyKid1 = Book("Diary of a Wimpy Kid", "Jeff Kinney", "Fiction", 2007, 217, 6.99, false)
-        gatsby = Book("The Great Gatsby", "Scott F. Fitzgerald", "Fiction", 1925, 176, 6.99, true)
+        wonder = Book("Wonder", "R. J. Palacio", "fiction", 2012, 315, 9.99, true)
+        noLongerHuman = Book("No Longer Human", "Osamu Dazai", "drama", 1948, 170, 7.99, true)
+        rollOfThunder = Book("Roll Of Thunder, Hear My Cry", "Mildred D. Taylor", "fiction", 1976, 220, 8.99, true)
+        wimpyKid1 = Book("Diary of a Wimpy Kid", "Jeff Kinney", "children", 2007, 217, 6.99, false)
+        gatsby = Book("The Great Gatsby", "Scott F. Fitzgerald", "drama", 1925, 176, 6.99, true)
 
-        // adding 5 Book to the books api
+        // adding 5 book to the books api
         populatedBooks!!.add(wonder!!)
         populatedBooks!!.add(noLongerHuman!!)
         populatedBooks!!.add(rollOfThunder!!)
@@ -55,11 +59,14 @@ class BookAPITest {
             assertTrue(populatedBooks!!.add(newBook))
             assertEquals(6, populatedBooks!!.numberOfBooks())
             assertEquals(newBook, populatedBooks!!.findBook(populatedBooks!!.numberOfBooks() - 1))
-        } }
-/*
+        }
+
         @Test
         fun `adding a Book to an empty list adds to ArrayList`() {
             val newBook = Book("test", "author", "science", 2002, 173, 4.99, false)
+            emptyBooks!!.clearArray()
+            //Manually flushing as objects would carry over from previous tests.
+
             assertEquals(0, emptyBooks!!.numberOfBooks())
             assertTrue(emptyBooks!!.add(newBook))
             assertEquals(1, emptyBooks!!.numberOfBooks())
@@ -68,25 +75,36 @@ class BookAPITest {
     }
 
     @Nested
-    inner class ListBooks {
-
+    inner class UpdateBooks {
         @Test
-        fun `listAllBooks returns No Books Stored message when ArrayList is empty`() {
-            assertEquals(0, emptyBooks!!.numberOfBooks())
-            assertTrue(emptyBooks!!.listAllBooks().lowercase().contains("no books stored"))
+        fun `updating a book that does not exist returns false`() {
+            emptyBooks!!.clearArray()
+            populatedBooks!!.clearArray()
+            assertFalse(populatedBooks!!.updateBook(6, Book("Wonder", "R. J. Palacio", "fiction", 2012, 315, 9.99, false)))
+            assertFalse(populatedBooks!!.updateBook(-1, Book("Wonder", "R. J. Palacio", "fiction", 2012, 315, 9.99, false)))
+            assertFalse(emptyBooks!!.updateBook(0, Book("Wonder", "R. J. Palacio", "fiction", 2012, 315, 9.99, false)))
         }
 
         @Test
-        fun `listAllBooks returns Books when ArrayList has books stored`() {
-            assertEquals(5, populatedBooks!!.numberOfBooks())
-            val booksString = populatedBooks!!.listAllBooks().lowercase()
-            assertTrue(booksString.contains("wonder"))
-            assertTrue(booksString.contains("no"))
-            assertTrue(booksString.contains("roll"))
-            assertTrue(booksString.contains("diary"))
-            assertTrue(booksString.contains("the"))
+        fun `updating a book that exists returns true and updates`() {
+            // check book 4 exists and check the contents
+            assertEquals(wimpyKid1, populatedBooks!!.findBook(4))
+            assertEquals("Diary of a Wimpy Kid", populatedBooks!!.findBook(4)!!.BookTitle)
+            assertEquals("Jeff Kinney", populatedBooks!!.findBook(4)!!.BookAuthor)
+            assertEquals("children", populatedBooks!!.findBook(4)!!.BookGenre)
+            assertEquals(2007, populatedBooks!!.findBook(4)!!.BookReleaseYear)
+            assertEquals(217, populatedBooks!!.findBook(4)!!.BookLength)
+            assertEquals(6.99, populatedBooks!!.findBook(4)!!.BookPrice)
+
+            // update note 5 with new information and ensure contents updated successfully
+            assertTrue(populatedBooks!!.updateBook(4, Book("Journal of a Simpy Kid", "Joe Kenny", "media", 2011, 327, 9.99, false)))
+            assertEquals("Journal of a Simpy Kid", populatedBooks!!.findBook(4)!!.BookTitle)
+            assertEquals("Joe Kenny", populatedBooks!!.findBook(4)!!.BookAuthor)
+            assertEquals("media", populatedBooks!!.findBook(4)!!.BookGenre)
+            assertEquals(2011, populatedBooks!!.findBook(4)!!.BookReleaseYear)
+            assertEquals(327, populatedBooks!!.findBook(4)!!.BookLength)
+            assertEquals(9.99, populatedBooks!!.findBook(4)!!.BookPrice)
         }
     }
 
- */
 }
